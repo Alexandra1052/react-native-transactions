9afaf21a15dd0bbf78e45dfe70e1b2b8577eacf5
Alexandra Ghita <anda.ghita05@gmail.com>
2025-06-28 02:16:10 +0300

    commit

--- backend/src/routes/transactionsRoute.js
+++ backend/src/routes/transactionsRoute.js
@@ -2,12 +2,12 @@ import express from 'express';
 import {createTransaction, getTransactionsByUserId,deleteTransaction, getSummaryByUserId} from '../controllers/transactionsController.js'; // Adjust the path as necessary
 const router = express.Router();
 
-
-router.get("/summary/:userId", getTransactionsByUserId);
+ 
+router.get("/summary/:userId",getSummaryByUserId );
 
 router.post("/", createTransaction);
 
 router.delete("/:id", deleteTransaction);
 
-router.get("/:userId",getSummaryByUserId );
+router.get("/:userId",getTransactionsByUserId);
 export default router;
